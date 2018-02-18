dd27dcdc6d320af0f3f40f74edcc61aad7bc607c
= <=>
2018-02-18 16:32:52 +0100

    Connexion par user

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skrechowiec
+ * Date: 18/02/2018
+ * Time: 15:02
+ */
+
+namespace App\Controller;
+
+
+class RegistrationController
+{
+
+}
---
+++ src/Form/UserType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skrechowiec
+ * Date: 18/02/2018
+ * Time: 15:01
+ */
+
+namespace App;
+
+
+class UserType
+{
+
+}
